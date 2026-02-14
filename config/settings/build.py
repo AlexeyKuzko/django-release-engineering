205bf3f5de2b2f56dbefb761c5d9b45db65b40a6
AlexeyKuzko <kuzkoalexey@icloud.com>
2026-02-14 16:45:30 +0300

    fixing build

--- config/settings/build.py
+++ config/settings/build.py
@@ -1,22 +1,13 @@
 import os
 
-from . import base as base_settings
-
 SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "build")
 
 DEBUG = False
 ALLOWED_HOSTS = ["*"]
 
-INSTALLED_APPS = base_settings.INSTALLED_APPS
-MIDDLEWARE = base_settings.MIDDLEWARE
-TEMPLATES = base_settings.TEMPLATES
-
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": ":memory:",
-    },
+    }
 }
-
-STATIC_URL = base_settings.STATIC_URL
-STATIC_ROOT = base_settings.STATIC_ROOT
