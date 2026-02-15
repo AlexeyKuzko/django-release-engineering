61230922229af58250cac8cbd4f44b72872286a0
AlexeyKuzko <kuzkoalexey@icloud.com>
2026-02-15 23:10:05 +0300

    fixing terraform

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,11 +1,17 @@
 terraform {
   backend "s3" {
-    endpoint   = "storage.yandexcloud.net"
-    bucket     = "diploma-terraform-state"
-    region     = "ru-central1"
-    key        = "prod/terraform.tfstate"
+    endpoints = {
+      s3 = "https://storage.yandexcloud.net"
+    }
+
+    bucket = "diploma-terraform-state"
+    region = "ru-central1"
+    key    = "prod/terraform.tfstate"
 
     skip_region_validation      = true
     skip_credentials_validation = true
+    skip_metadata_api_check     = true
+    skip_requesting_account_id  = true
+    skip_s3_checksum            = true
   }
 }
