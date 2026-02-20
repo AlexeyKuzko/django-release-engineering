bd2fff21c9a731eb78eb4b0d72ce8e40b09c69ce
AlexeyKuzko <kuzkoalexey@icloud.com>
2026-02-20 14:45:24 +0300

    add needs, fixing ansible

--- infra/network.tf
+++ infra/network.tf
@@ -2,6 +2,22 @@ resource "yandex_vpc_network" "main" {
   name = "diploma-network"
 }
 
+resource "yandex_vpc_gateway" "nat" {
+  name = "nat-gateway"
+
+  shared_egress_gateway {}
+}
+
+resource "yandex_vpc_route_table" "private" {
+  name       = "private-rt"
+  network_id = yandex_vpc_network.main.id
+
+  static_route {
+    destination_prefix = "0.0.0.0/0"
+    gateway_id         = yandex_vpc_gateway.nat.id
+  }
+}
+
 resource "yandex_vpc_subnet" "public" {
   name           = "public-subnet"
   zone           = var.zone
@@ -13,5 +29,6 @@ resource "yandex_vpc_subnet" "private" {
   name           = "private-subnet"
   zone           = var.zone
   network_id     = yandex_vpc_network.main.id
+  route_table_id = yandex_vpc_route_table.private.id
   v4_cidr_blocks = [var.private_cidr]
 }
