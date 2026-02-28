4b982de2f6122981d39ac848a75bfe5f8dc61a19
AlexeyKuzko <kuzkoalexey@icloud.com>
2026-02-28 18:46:49 +0300

    wip on app logics

--- django_educational_demo_application/users/apps.py
+++ django_educational_demo_application/users/apps.py
@@ -1,6 +1,8 @@
 from django.apps import AppConfig
 from django.utils.translation import gettext_lazy as _
 
+from . import signals  # noqa: F401
+
 
 class UsersConfig(AppConfig):
     name = "django_educational_demo_application.users"
@@ -8,4 +10,3 @@ class UsersConfig(AppConfig):
 
     def ready(self):
         """Import signals when Django starts."""
-        from . import signals  # noqa: F401
